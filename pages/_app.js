79320c63e68d3e3579d7601aab127416d517c103
cryncyn <cryncyn@gmail.com>
2020-08-05 14:54:14 +0530

    deploy ready i guess

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(<>
+  <header>
+    <h1>greetings</h1>
+    <nav>
+
+    </nav>
+  </header>
+  <Component {...pageProps} />
+  </>);
 }
 
 export default MyApp
